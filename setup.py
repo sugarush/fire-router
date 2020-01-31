41667ec996155a1cb408b05c5e1d2fc691047c98
Paul Severance <paul.severance@gmail.com>
2020-01-31 12:04:34 -0500

    rename to fire-router

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-__author__ = 'Lucifer Avada'
+__author__ = 'Paul Severance'
 
 from setuptools import setup
 
 setup(
-    name='sugar-router',
+    name='fire-router',
     version='0.0.1',
-    author='Lucifer Avada',
-    author_email='lucifer.avada@gmail.com',
-    url='https://github.com/sugarush/sugar-router',
+    author='Paul Severance',
+    author_email='paul.severance@gmail.com',
+    url='https://github.com/sugarush/fire-router',
     packages=[
-        'sugar_router'
+        'fire_router'
     ],
     description='A lightweight event router.',
     install_requires=[
-        'sugar_asynctest'
+        'fire_asynctest'
     ],
     dependency_links=[
-        'git+https://github.com/sugarush/sugar-asynctest@master#egg=sugar-asynctest'
+        'git+https://github.com/sugarush/fire-asynctest@master#egg=fire-asynctest'
     ]
 )
